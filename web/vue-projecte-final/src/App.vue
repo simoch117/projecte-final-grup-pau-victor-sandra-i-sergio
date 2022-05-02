0ce752168ba064beb9b44d3a0108cabfee8aefe0
AVictorMoreno <a20vicmormor@inspedralbes.cat>
2022-05-02 08:47:40 +0200

    create vue

---
+++ web/vue-projecte-final/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+import HomeEjercicio from "@/view/HomeEjercicio.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/ejercicio">Ejercicio</RouterLink>
+        <RouterLink to="/alimentacion">Alimentación</RouterLink>
+        <RouterLink to="/saludmentalydescanso"
+          >Salud Mental y Descanso</RouterLink
+        >
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+</style>
---
+++ web/vue-projecte-final/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/ejercicio",
+      name: "ejercicio",
+      component: HomeEjercicio,
+    },
+    {
+      path: "/alimentacion",
+      name: "alimentacion",
+      component: HomeAlimentacion,
+    },
+    {
+      path: "/saludmentalydescanso",
+      name: "saludmentalydescanso",
+      component: HomeSaludmentalydescanso,
+    },
+  ],
+});
+
+export default router;
